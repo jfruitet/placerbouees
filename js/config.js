5a6f6fd582d305817afdcfe7a7f29f75ad15d5d1
Jean FRUITET <jean.fruitet@free.fr>
2023-08-06 09:04:59 +0200

    Données de configuration pour le serveur
    Signed-off-by: Jean FRUITET <jean.fruitet@free.fr>

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+let url_serveur = 'http://localhost/placerbouees/php/';     // Les scripts des serveurs
+let url_data = 'http://localhost/placerbouees/data/';   // les fichiers de sortie 
+let url_json = 'http://localhost/placerbouees/json/';   // les fichiers de configuration des plans d'eau
+
