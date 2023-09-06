de3e2fbd54607a2c8c01166c8da58772c99351c4
Jean FRUITET <jean.fruitet@free.fr>
2023-09-06 10:47:15 +0200

    Amélioration de la sélection
    Signed-off-by: Jean FRUITET <jean.fruitet@free.fr>

--- js/ajax3.js
+++ js/ajax3.js
@@ -50,12 +50,22 @@ function ajax_getDisplaySites(url, mystr){
         // .then(response => console.debug(response))
         .then((data) => {
             for (const site of data.site) {
+                //console.debug(site);
                 const listItem = document.createElement("li");
+                
                 var bouton = document.createElement('button');
-                bouton.setAttribute('name', `${site.id}`);                
-                bouton.setAttribute('onclick', `getThatPlansEau(${site.id})`);
-                bouton.textContent = site.id;
+                bouton.setAttribute('name', `Choix${site.id}`);                
+                bouton.setAttribute('onclick', `selectThatPlanEau("${site.name}")`);                
+                bouton.textContent = 'Afficher';
                 listItem.appendChild(bouton);
+                
+                // Générer les fichiers auto                
+                var bouton2 = document.createElement('button');
+                bouton2.setAttribute('name', `${site.id}`);                
+                bouton2.setAttribute('onclick', `getThatPlanEau(${site.id})`);
+                bouton2.textContent = 'Générer';
+                listItem.appendChild(bouton2);
+                
                 // listItem.appendChild(document.createElement("strong")).textContent = site.id;
                 listItem.append(` ${site.name},  ${site.club}, `);
                 listItem.appendChild(
@@ -87,10 +97,21 @@ function getListePlansEau(){
 }
 
     
+// ----------------------- 
+function selectThatPlanEau(sname){
+    if (sname !== undefined){
+        console.debug("selectThatPlanEau");
+        console.debug(sname);  
+        getDataSite(sname);
+        nomDuSite=sname;
+        document.getElementById("info").innerHTML=sname;  
+        setCookies();
+    }
+}    
 
 // ----------------------- 
-function getThatPlansEau(id_plan_eau){  
-    //console.debug("getThatPlansEau("+id_plan_eau+")"); 
+function getThatPlanEau(id_plan_eau){  
+    //console.debug("getThatPlanEau("+id_plan_eau+")"); 
     var url= url_serveur+'plans_eau.php';
     var myget="all=0&id="+id_plan_eau;
     ajax_SetPlanEau(url, myget);    
@@ -160,21 +181,6 @@ function ajax_GetDataWindSite(url, mystr){
         })                          
         .catch(error => console.debug("Erreur : "+error));
     }
-}
-
-
-//-----------------
-// function proposeChoixData(){    // Tous les fichiers de data
-function afficheDataDispo(){    // Tous les fichiers de data
-    if ((tFichiers !== undefined) && (tFichiers.length>0)){ 
-        //console.debug("tFichiers 2 : "+ tFichiers);
-        document.getElementById("myListData").innerHTML=""; // Sinon les items fichiers sont en doublés 
-                                                            // et je n'ai toujours pas compris pourquoi
-        for (var i=0; i<tFichiers.length; i++){     
-            //console.debug("tFichiers i:"+i+" => "+ tFichiers[i]);
-            document.getElementById("myListData").innerHTML+="<li>"+tFichiers[i]+"</li>";
-        }
-    }                     
 }
 
 // ----------------------- 
@@ -297,4 +303,96 @@ function setSiteBoueesMobile(site){
     }       
 
 } 
+
+
+
+    
+// Affiche la liste des données validées
+// ----------------------- 
+function getValidatedData(){  
+    //console.debug("getValidatedData()");
+    var url= url_serveur+'get_validated.php';
+    ajax_GetValidatedData(url);     
+}
+
+
+// Affichage de la liste dans un tableau
+// ----------------------- 
+function ajax_GetValidatedData(url){ 
+    if ((url !== undefined) && (url.length>0)){        
+        // GET avec fetch()
+        fetch(url, myInitGet)
+        .then(response => response.json()) 
+        .then((data) => {
+                if (data.valide !== undefined){
+                    var str="<table><tr><th>Id</th><th>Fichier</th><th>Id</th><th>Fichier</th><th>Id</th><th>Fichier</th></tr>";
+                    
+                    for( var index=0; index<data.valide.length; index++){
+                        if ((index % 3) == 0){
+                            str+="<tr>";
+                        }
+                        str+="<td>"+data.valide[index].id+"</td><td><a href=\"data/"+data.valide[index].file+"\">"+data.valide[index].file+"</a></td>";
+                        if ((index % 3) == 2){
+                            str+="</tr>";
+                        }                        
+                    }             
+                    if ((index % 3) == 1){
+                        str+="<td>&nbsp;</td></tr>";
+                    }                        
+                    str+="</table>";
+                    document.getElementById("fvalide").innerHTML=str;   
+                }
+            }
+        )           
+        .catch(error => console.debug("Erreur : "+error));
+    }
+}
+
+
+//-----------------
+// function proposeChoixData(){    // Tous les fichiers de data
+function afficheDataDispo(){    
+    if ((tFichiers !== undefined) && (tFichiers.length>0)){ 
+        //console.debug("tFichiers 2 : "+ tFichiers);
+        document.getElementById("myListData").innerHTML=""; // Sinon les items fichiers sont en doublés 
+        if ((tFichiers !== undefined) && (tFichiers.length>0)){
+            var compteur1=0;
+            var compteur2=0;
+            var str1="<h4>Placements vérifiés</h4>\n<table><tr><th>Id</th><th>Fichier</th><th>Id</th><th>Fichier</th><th>Id</th><th>Fichier</th></tr>";
+            var str2="<h4>Placements automatiques</h4>\n<table><tr><th>Id</th><th>Fichier</th><th>Id</th><th>Fichier</th><th>Id</th><th>Fichier</th></tr>";        
+                for( var index=0; index<tFichiers.length; index++){
+                    if (tFichiers[index].match("auto")){
+                        if ((compteur2 % 3) == 0){
+                            str2+="<tr>";
+                        }
+                        str2+="<td>"+compteur2+"</td><td><a href=\"data/"+tFichiers[index]+"\">"+tFichiers[index]+"</a></td>";
+                        if ((compteur2 % 3) == 2){
+                            str2+="</tr>";
+                        }             
+                        compteur2++;
+                    }
+                    else{
+                        if ((compteur1 % 3) == 0){
+                            str1+="<tr>";
+                        }
+                        str1+="<td>"+compteur1+"</td><td><a href=\"data/"+tFichiers[index]+"\">"+tFichiers[index]+"</a></td>";
+                        if ((compteur1 % 3) == 2){
+                            str1+="</tr>";
+                        }             
+                        compteur1++;                    
+                    }               
+                }             
+                if ((compteur2 % 3) == 1){
+                    str2+="<td>&nbsp;</td></tr>";
+                }                        
+                if ((compteur1 % 3) == 1){
+                    str1+="<td>&nbsp;</td></tr>";
+                }
+                str1+="</table>";                        
+                str2+="</table>";
+                document.getElementById("myListData").innerHTML=str1+"\n"+str2;           
+        }      
+    }                     
+}
+
 
